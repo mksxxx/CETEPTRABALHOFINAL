3e03ef775ad8820c6ce524e886e078b533d66be4
mksxxx <144238830+mksxxx@users.noreply.github.com>
2023-09-18 09:11:25 -0300

    Add files via upload

---
+++ a/app.js
@@ -0,0 +1,11 @@
+//cabeçalho
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click" , () => nav.classList.toggle
+("active"));
+
+//fim cabeçalho
+
+
+//fim banner
